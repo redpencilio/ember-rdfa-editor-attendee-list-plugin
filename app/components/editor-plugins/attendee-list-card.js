6577cbf2f94f0c2eb53fbabf9b20fd3b604a3f1d
Mehran Naghizadeh <tarhineh@gmail.com>
2019-08-29 17:31:20 +0200

    Generate the plugin scaffold

---
+++ app/components/editor-plugins/attendee-list-card.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-rdfa-editor-attendee-list-plugin/components/editor-plugins/attendee-list-card';
---
+++ app/services/rdfa-editor-attendee-list-plugin.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-rdfa-editor-attendee-list-plugin/services/rdfa-editor-attendee-list-plugin';
